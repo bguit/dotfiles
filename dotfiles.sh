d58bf4283604fcdd53d708061836dda6dcf86c4f
Zzzzuuu <bguit@github.com>
2020-05-24 12:18:42 -0400

    Initial

---
+++ dotfiles.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 install|update"
+    exit -1
+fi
+
+case $1 in 
+    "install" ) 
+        cp ./files/tmux.backup ~/.tmux.conf
+        cp ./files/vimrc.backup ~/.vimrc
+        cp ./files/custom.zsh-theme.backup ~/.oh-my-zsh/themes/custom.zsh-theme
+        echo "Install: Done!"
+    ;;
+    "update" ) 
+        cp ~/.tmux.conf ./files/tmux.backup
+        cp ~/.vimrc ./files/vimrc.backup
+        cp ~/.oh-my-zsh/themes/custom.zsh-theme ./files/custom.zsh-theme.backup
+        echo "Update: Done!"
+    ;;
+    * )
+        echo "Usage: $0 install|update"
+        exit -1
+    ;;
+esac
