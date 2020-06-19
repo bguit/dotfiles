836a936ad301f7476749802acd5a87be9851cd5b
Zzzzuuu <bguit@github.com>
2020-06-19 10:34:22 -0400

    Update: script

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 if [ $# -ne 1 ]; then
-    echo "Usage: $0 install|update"
+    echo "Usage: $0 fromlocal|tolocal"
     exit -1
 fi
 
 case $1 in 
-    "install" ) 
+    "tolocal" ) 
         cp ./files/tmux.backup ~/.tmux.conf
         cp ./files/vimrc.backup ~/.vimrc
         cp ./files/custom.zsh-theme.backup ~/.oh-my-zsh/themes/custom.zsh-theme
         echo "Install: Done!"
     ;;
-    "update" ) 
+    "fromlocal" ) 
         cp ~/.tmux.conf ./files/tmux.backup
         cp ~/.vimrc ./files/vimrc.backup
         cp ~/.oh-my-zsh/themes/custom.zsh-theme ./files/custom.zsh-theme.backup
