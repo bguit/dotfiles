33a1ffda76b436074f5bcb1c26bccea10bea4d02
Zzzzuuu <bguit@github.com>
2020-08-26 07:51:15 -0400

    Update

--- dotfiles.sh
+++ dotfiles.sh
@@ -10,12 +10,14 @@ case $1 in
         cp ./files/tmux.backup ~/.tmux.conf
         cp ./files/vimrc.backup ~/.vimrc
         cp ./files/zshrc.backup ~/.zshrc
+        cp ./files/lscolors.sh.backup ~/.local/share/lscolors.sh
         cp ./files/custom.zsh-theme.backup ~/.oh-my-zsh/themes/custom.zsh-theme
         echo "Install: Done!"
     ;;
     "fromlocal" ) 
         cp ~/.tmux.conf ./files/tmux.backup
         cp ~/.zshrc ./files/zshrc.backup
+        cp ~/.local/share/lscolors.sh ./files/lscolors.sh.backup
         cp ~/.vimrc ./files/vimrc.backup
         cp ~/.oh-my-zsh/themes/custom.zsh-theme ./files/custom.zsh-theme.backup
         echo "Update: Done!"
